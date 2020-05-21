301c8365a86e0997f7bfde079ab369f887507994
Yrvin Escorihuela <yrvin.escorihuela@gmail.com>
2020-05-20 23:51:21 -0400

    Fix port setting

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,7 +4,6 @@ import { FETCH_USERS } from 'actions/types';
 const apiUrl = process.env.BACKEND_API_URL;
 
 export const fetchUsers = async () => {
-  console.log(apiUrl);
   users = await axios.get(apiUrl);
 
   return {
