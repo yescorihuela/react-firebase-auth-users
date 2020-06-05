df477984aed5f149061e72928efad1ad4237c6ce
Yrvin Escorihuela <yrvin.escorihuela@gmail.com>
2020-06-05 12:47:05 -0400

    Fix missing dependencies

--- src/Root.js
+++ src/Root.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import generateStore from 'redux/store';
+import generateStore from 'reducers';
 const store = generateStore();
 export default ({children}) => {
   return (
